28765cfd589226d7624630c7917ca9510def04b4
abialvarado14 <suyapa.caldex@hotmail.es>
2020-11-19 16:19:10 -0600

    Paginación

--- pokemonweb/src/component/Home/Home.jsx
+++ pokemonweb/src/component/Home/Home.jsx
@@ -4,10 +4,37 @@ import logopokemon from './logopokemon.png';
 import { Jumbotron, Container, Button} from 'react-bootstrap';
 import Pokemon from '../Pokemon/Pokemon';
 
-export default class Home extends Component {
+
+const Home = () =>{
+
+
+    return (
+        <Container>
+            <div className="outer-div">
+                <div className="jumbotron-div">
+                    <Jumbotron className="jumbo-boy" >
+                        <h1>Registro </h1>
+                        <img src={logopokemon} className="logo" alt="logo" />
+                        <h5>¡Encuentra la información de tu pokemon favorito!</h5>
+                    </Jumbotron>
+                </div>
+
+            </div>
+          <br />
+          <Pokemon />
+        </Container>           
+        
+    );
+
+
+}
+
+export default Pokemon
+
+
+/*export default class Home extends Component {
    
     render() {
-
         return (
             <Container>
                 <div className="outer-div">
@@ -22,11 +49,14 @@ export default class Home extends Component {
                 </div>
               <br />
               <Pokemon />
-            </Container>
-            
+
+              <Button type = "submit" className="float-right" variant="outline-warning" onClick={handleClick}>
+                Siguiente
+             </Button>
+            </Container>           
             
         );
     }
-}
+}*/
 
 
--- pokemonweb/src/component/Pokemon/Pokemon.jsx
+++ pokemonweb/src/component/Pokemon/Pokemon.jsx
@@ -5,9 +5,14 @@ import './Pokemon.css';
 
 
 const Pokemon = () => {
-    const[ pokemonlista, setPokemonlista] = useState([])
+    const[ pokemonlista, setPokemonlista] = useState([]) //Lista donde cargaremos la API
+    const [siguiente, setSiguiente] = useState(null); //La página siguiente de la API
+    const [anterior, setAnterior] = useState(null); //La página anterior de la API
+    const [actual, setActual] = useState('https://pokeapi.co/api/v2/pokemon'); //La página actual de la API
 
     useEffect(() =>{
+        console.log(actual)
+        console.log(pokemonlista)
         var myHeaders = new Headers();
 
         var myInit = {
@@ -17,15 +22,34 @@ const Pokemon = () => {
             cache: 'default'
         };
 
-        fetch('https://pokeapi.co/api/v2/pokemon', myInit)
+        fetch(actual, myInit)
             .then(res => {
                 return res = res.json();
             })
             .then(data => {
                 setPokemonlista(data.results)
+                setSiguiente(data.next)
+                setAnterior(data.previous)
             });
+            pokemonlista.map((pokemon) => {
+                return(
+                    <Card border="warning" bg="dark" text="white">
+                            <Card.Body>
+                                <Card.Title>
+                                    {pokemon.name}
+                                    <Button className="float-right" variant="outline-warning"><img src={pokeball} className="pokeball" alt="pokeball" />
+                                        Información
+                                    </Button>
+                                </Card.Title>
+    
+                            </Card.Body>
+                        </Card>
+                )
+    
+            })
+    }, [actual]);
+      
 
-    }, []);
 
     return (
         <div>
@@ -45,6 +69,13 @@ const Pokemon = () => {
             )
 
         })}
+        
+        <Button type = "submit" className="float-right" variant="outline-warning" onClick={()=>setActual(siguiente)}>
+            Siguiente
+         </Button>
+         <Button type = "submit" className="float-left" variant="outline-warning" onClick={()=>setActual(anterior)}>
+            Anterior
+         </Button>
         </div>  
     );
 }
